616b3c810a8b0c9ac6c66143beb1a696e951f656
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-07 09:54:17 +0900

    p71 timesメソッドを親クラスへ

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -9,8 +9,8 @@ public class Money {
         this.currency = currency;
     }
 
-    Money times(int amount) {
-        return null;
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 
     String currency() {
