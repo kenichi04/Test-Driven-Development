2b64ac063cc8137503119e3756f9f8ae45bc86b9
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-08 07:42:42 +0900

    p86 通貨加算処理のための仮実装

---
+++ src/main/java/money/Bank.java
@@ -0,0 +1,8 @@
+package money;
+
+public class Bank {
+    Money reduce(Expression source, String to) {
+        // TODO testを通すための仮実装
+        return Money.dollar(10);
+    }
+}
--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -13,7 +13,7 @@ public class Money {
         return new Money(amount * multiplier, currency);
     }
 
-    Money plus(Money added) {
+    Expression plus(Money added) {
         return new Money(amount + added.amount, currency);
     }
 
