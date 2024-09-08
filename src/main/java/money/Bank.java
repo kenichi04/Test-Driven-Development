704b7714cd08634f519c685b66dd5e1871e27b5c
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-08 09:36:51 +0900

    Bank#reduce()にMoneyインスタンスを渡すテスト追加（要改修）

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -3,6 +3,7 @@ package money;
 public class Bank {
     Money reduce(Expression source, String to) {
         // TODO fixme キャストなくす
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
