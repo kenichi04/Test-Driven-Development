dba17ae2c84b77bd4f3fa43bd3a4f7efe72d564f
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-08 09:40:17 +0900

    p95 ポリモフィズム置き換えのため、Moneyに同じメソッド実装

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,8 +2,8 @@ package money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        // TODO fixme キャストなくす
-        if (source instanceof Money) return (Money) source;
+        // TODO fixme キャストなくす > ポリモフィズムへ置き換え
+        if (source instanceof Money) return ((Money) source).reduce(to);
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
