169343c9b38470a5f9cf7b0d69810eadfa95a6f6
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-09 13:10:54 +0900

    p102 通貨換算テストおよび仮実装（要改修）

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -18,7 +18,9 @@ public class Money implements Expression {
     }
 
      public Money reduce(String to) {
-        return this;
+        // TODO 仮実装
+         int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+         return new Money(amount / rate, to);
     }
 
     String currency() {
